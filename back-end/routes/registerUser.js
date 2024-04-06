6802c5bd3b4d435a890e726a145b512513ab235d
anesushangwa01 <anesushangwa01@gmail.com>
2024-04-06 13:01:51 +0200

    updated

--- back-end/routes/registerUser.js
+++ back-end/routes/registerUser.js
@@ -5,10 +5,10 @@ const register = require('../models/register');
 // POST a new information
 router.post("/", async (req, res) => {
     try {
-      const application = new  JobApplicationModel(req.body); // Assuming req.body contains the product data
+      const application = new  register(req.body); // Assuming req.body contains the product data
       await application.save();
       
-      res.status(201).json({ message: 'applicaation submited ',  });
+      res.status(201).json({ message: ' user created  ',  });
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
